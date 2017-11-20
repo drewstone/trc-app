0b4013e860fd896065c2024031f488323726b586
Drew Stone <drewstone329@gmail.com>
2017-11-20 15:46:25 -0500

    Add prediction + question

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,6 @@
-import { combineReducers } from 'redux';
+
+
+fdsjfdksljfdsklfdsjkljkljkjkimport { combineReducers } from 'redux';
 import { screens, screenActions, marketActions } from '../constants';
 import { handle } from 'redux-pack';
 
@@ -43,6 +45,66 @@ const marketReducer = (state = marketInitialState, action) => {
         finish: prevState => ({ ...prevState, isLoading: false }),
         failure: prevState => ({ ...prevState, fooError: payload }),
         success: prevState => ({ ...prevState, questions: payload.questions }),
+      });
+    default:
+      return state;
+  }
+}
+
+export default combineReducers({
+  currentScreen: screenReducer,
+  market: marketReducer,
+});
+import { combineReducers } from 'redux';
+import { screens, screenActions, marketActions } from '../constants';
+import { handle } from 'redux-pack';
+
+const screenReducer = (state = screens.NEWUSER, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case screenActions.SWITCH_TO:
+      return payload;
+    default:
+      return state;
+  }
+};
+
+const marketInitialState = {
+  predictions: [],
+  questions: [],
+};
+
+const marketReducer = (state = marketInitialState, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case marketActions.ADD_QUESTION:
+      return handle(state, action, {
+        start: prevState => ({ ...prevState, isLoading: true, fooError: null }),
+        finish: prevState => ({ ...prevState, isLoading: false }),
+        failure: prevState => ({ ...prevState, fooError: payload }),
+        success: prevState => ({
+          ...prevState,
+          questions: [ ...prevState.questions, payload.question ],
+        }),
+      });
+    case marketActions.FETCH_QUESTIONS:
+      return handle(state, action, {
+        start: prevState => ({ ...prevState, isLoading: true, fooError: null }),
+        finish: prevState => ({ ...prevState, isLoading: false }),
+        failure: prevState => ({ ...prevState, fooError: payload }),
+        success: prevState => ({ ...prevState, questions: payload.questions }),
+      });
+    case marketActions.ADD_PREDICTION:
+      return handle(state, action, {
+        start: prevState => ({ ...prevState, isLoading: true, fooError: null }),
+        finish: prevState => ({ ...prevState, isLoading: false }),
+        failure: prevState => ({ ...prevState, fooError: payload }),
+        success: prevState => ({
+          ...prevState,
+          predictions: [ ...prevState.predictions, payload.prediction ]
+        }),
       });
     default:
       return state;
